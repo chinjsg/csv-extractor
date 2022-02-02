e70943ac34bdfbd8b645a9184b99aff784ab46d2
chinjsg <dragvern@hotmail.com>
2022-02-01 23:58:26 -0700

    added append functionality
    Allow option selection to either generate new csv file or update existing csv file

--- csv_extractor.py
+++ csv_extractor.py
@@ -8,7 +8,7 @@
     Notes:
         - Currently supports retrieving COVID-19 data from Singapore only
         - Extracted data is written into new csv file 'covid19_singapore.csv' within the same directory
-        - Please backup file and/or ensure file is not directly used in other projects as this file will be overwritten
+        - Two options: Generate new csv or update previous created csv
 """
 
 from datetime import datetime
@@ -64,13 +64,64 @@ def process(row, indexes, datestr):
 
     return [datestr, country, confirmed, deaths, recovered, active]
 
+def append_contents(writer, filenames, file_url):
+    """
+    Sends a request for the files in the Git repository, retrieve, and write the information to csv 
+    Parameters:
+        writer: the file writer object
+        filenames: the list of file names to search and write into the csv
+        file_url: URL to Github repository
+    """
+    # For each file
+    for fname in filenames:
+        print(fname)
+        furl = file_url + fname
+        response = requests.get(furl, timeout=5)
+        response_decoded = [line.decode('utf-8') for line in response.iter_lines()]
+        contents = list(csv.reader(response_decoded))
+        
+        # Get column names and determine old/new format of file
+        column_names = contents[0]
+        format_new = False
+        if len(column_names) > 8:
+            format_new = True
+
+        column_index = create_dict(column_names, format_new)
+
+        # Write row to the csv file
+        for row in contents:
+            if row[column_index['Country_Region']] == 'Singapore':
+                record = process(row, column_index, fname.split('.')[0])
+                writer.writerow(record)
 
 def main():
     url = "https://api.github.com/repos/CSSEGISandData/COVID-19/contents/csse_covid_19_data/csse_covid_19_daily_reports"
     file_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/"
     filenames = []
 
-    response = requests.get(url, timeout=5)
+
+    # User prompt selection
+    prompt = "Choose method by enter the corresponding number:\n1) Generate new CSV file\n2) Append existing CSV file\n"
+    choice = input(prompt)
+    while choice not in ('1', '2'):
+        print("Invalid option. Please enter again:")
+        choice = input(prompt)
+
+
+    try:
+        response = requests.get(url, timeout=10)
+    except requests.exceptions.Timeout:
+        # Timeout
+        print('Connection timeout')
+        sys.exit(-1)
+    except requests.exceptions.TooManyRedirects:
+        # Notify user of invalid URL
+        print('Bad URL. Check URL and retry')
+    except requests.exceptions.RequestException as e:
+        # Error
+        print('An error occured')
+        raise SystemExit(e)
+
     directory = response.json()
 
     for file in directory:
@@ -79,32 +130,32 @@ def main():
 
     filenames.sort(key = lambda fname: datetime.strptime(fname.split('.')[:1][0], '%m-%d-%Y'))
 
-    with open(os.path.join(sys.path[0], 'covid19_singapore_copy.csv'), 'w', newline='') as f:
-        # Create the csv writer and write column headers
-        writer = csv.writer(f)
-        writer.writerow(['Date', 'Country_Region','Confirmed', 'Deaths', 'Recovered', 'Active'])
+    if choice == '1':
+        with open(os.path.join(sys.path[0], 'covid19_singapore_copy.csv'), 'w', newline='') as f:
+            # Create the csv writer and write column headers
+            writer = csv.writer(f)
+            writer.writerow(['Date', 'Country_Region','Confirmed', 'Deaths', 'Recovered', 'Active'])
+            append_contents(writer, filenames, file_url)
 
-        # For each file
-        for fname in filenames:
-            print(fname)
-            furl = file_url + fname
-            response = requests.get(furl, timeout=5)
-            response_decoded = [line.decode('utf-8') for line in response.iter_lines()]
-            contents = list(csv.reader(response_decoded))
-            
-            # Get column names and determine old/new format of file
-            column_names = contents[0]
-            format_new = False
-            if len(column_names) > 8:
-                format_new = True
+    elif choice == '2':
+        # Identify last recorded date and append newest
+        with open(os.path.join(sys.path[0], 'covid19_singapore_copy.csv'), "r") as csvfile:
+            csvreader = csv.reader(csvfile)
+            for line in csvreader:
+                pass
+        last_date_str = line[0] + '.csv'
+        index = filenames.index(last_date_str)
+        filenames = filenames[index+1:]
 
-            column_index = create_dict(column_names, format_new)
-
-            # Write row to the csv file
-            for row in contents:
-                if row[column_index['Country_Region']] == 'Singapore':
-                    record = process(row, column_index, fname.split('.')[0])
-                    writer.writerow(record)
+        with open(os.path.join(sys.path[0], 'covid19_singapore_copy.csv'), 'a', newline='') as f:
+            # Create the csv writer
+            writer = csv.writer(f)
+            append_contents(writer, filenames, file_url)
+    else:
+        print('Something went wrong')
+        sys.exit(-1)
+    
+    print("Completed")
 
 if __name__ == "__main__":
     main()
